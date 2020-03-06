01b6c9568ff6f6e0b8f94730f04521da53c60886
seungchulsong <58935840+seungchulsong@users.noreply.github.com>
2020-03-06 18:06:03 +0900

    페이지 (상수)
    1. 게시판, 댓글, 공지사항

---
+++ 20200305_SpringBoard(new)/src/main/java/myutil/MyConstant.java
@@ -0,0 +1,27 @@
+package myutil;
+
+public class MyConstant {
+
+	//게시판 상수
+	public static class Board{
+		
+		public static final int BLOCKLIST = 10;
+		public static final int BLOCKPAGE = 3;
+	}
+	
+	//댓글관련 상수
+	public static class Comment{
+		
+		public static final int BLOCKLIST = 3;
+		public static final int BLOCKPAGE = 3;
+		
+	}
+	
+	//공지사항
+	public static class Note{
+		
+		public static final int BLOCKLIST = 5;
+		public static final int BLOCKPAGE = 3;
+	}
+	
+}
